a2c2ccbf11747f61b4688a35ec52e0ec5fc4dc55
Andy Black <Sprotex@gmail.com>
2020-04-18 11:15:41 +0200

    add beginning project boilerplate

---
+++ Assets/FollowTarget.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset = Vector3.zero;
+    public float interpolationSpeed = 1f;
+    
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position + offset, Time.deltaTime * interpolationSpeed);
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float rotateSpeed = 5f;
+
+    private bool isFlying = false;
+    private Vector3 movement = Vector3.zero;
+
+    private void HandleInputs()
+    {
+        if (Input.GetButtonDown("Fly"))
+        {
+            isFlying = !isFlying;
+        }
+        var horizontalMovement = Input.GetAxis("Horizontal");
+        var verticalMovement = Input.GetAxis("Vertical");
+        movement.x = horizontalMovement;
+        movement.z = verticalMovement;
+        if (movement.sqrMagnitude > 1f)
+        {
+            movement.Normalize();
+        }
+    }
+
+    private void Move()
+    {
+        transform.Translate(movement * moveSpeed * Time.deltaTime);
+    }
+
+    private void Update()
+    {
+        HandleInputs();
+        Move();
+    }
+}

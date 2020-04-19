9495e2dfa89ef7e07e8efcb494ee884840de5740
Andy Black <Sprotex@gmail.com>
2020-04-19 09:58:01 +0200

    add basic menu

---
+++ Assets/GameMenuLogic.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenuLogic : MonoBehaviour
+{
+    public int mainMenuBuildIndex = 0;
+    public GameObject menuPanel;
+    public PlayerAttack playerAttack;
+    public PlayerMovement playerMovement;
+    public PlayerFeed playerFeed;
+    private void Start()
+    {
+        menuPanel.SetActive(true);
+        DisplayMenu();
+    }
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene(mainMenuBuildIndex);
+    }
+    public void DisplayMenu()
+    {
+        var activeState = !menuPanel.activeSelf;
+        menuPanel.SetActive(activeState);
+        playerAttack.enabled = !activeState;
+        playerMovement.enabled = !activeState;
+        playerFeed.enabled = !activeState;
+        Time.timeScale = activeState ? 0f : 1f;
+        Cursor.lockState = activeState ? CursorLockMode.None : CursorLockMode.Locked;
+    }
+    private void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            DisplayMenu();
+        }
+    }
+}
---
+++ Assets/MainMenuLogic.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuLogic : MonoBehaviour
+{
+    public int gameSceneBuildIndex = 1;
+    public GameObject creditsPanel;
+    public GameObject mainPanel;
+    private GameObject activePanel;
+    private void Start()
+    {
+        creditsPanel.SetActive(false);
+        activePanel = mainPanel;
+        activePanel.SetActive(true);
+    }
+    public void NewGame()
+    {
+        SceneManager.LoadScene(gameSceneBuildIndex);
+    }
+    private void SwitchToPanel(GameObject panel)
+    {
+        activePanel.SetActive(false);
+        activePanel = panel;
+        activePanel.SetActive(true);
+    }
+    public void Credits()
+    {
+        SwitchToPanel(creditsPanel);
+    }
+    public void BackToMenu()
+    {
+        SwitchToPanel(mainPanel);
+    }
+}

d6e0a53c496afe2717779723be4ec136c4c7f4ef
Andy Black <Sprotex@gmail.com>
2020-04-19 10:56:58 +0200

    add tutorial logic

---
+++ Assets/Scripts/TutorialLogic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialLogic : MonoBehaviour
+{
+    public GameObject movePanel;
+    public GameObject flyPanel;
+    public GameObject attackPanel;
+    public GameObject pickupPanel;
+    public GameObject feedPanel;
+    private List<GameObject> tutorialSteps = new List<GameObject>();
+    private int tutorialStepIndex = 0;
+
+    private void Start()
+    {
+        if (PlayerPrefs.GetInt("Tutorial") == 1)
+        {
+            PlayerPrefs.SetInt("Tutorial", 0);
+        }
+    }
+}
